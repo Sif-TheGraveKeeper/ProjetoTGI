610a4dda3c785598ebb07130ef26fa7c9fa51b53
Guilherme Carini <carinigcontact@gmail.com>
2021-03-25 19:16:22 -0300

    pastas iniciais recicladas de PunkHazard

---
+++ Code/Resposta.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Resposta 
+{
+    public string resposta;
+    public FalaNPC proximaFala;
+}
