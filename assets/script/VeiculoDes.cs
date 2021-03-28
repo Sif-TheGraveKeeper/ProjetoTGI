ea6260a17f65f74ee2fd8445e28bc89975b11613
Guilherme Carini <carinigcontact@gmail.com>
2021-03-28 11:46:08 -0300

    Implementando player

---
+++ assets/script/VeiculoDes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeiculoDes : MonoBehaviour
+{
+    private bool colidde = false;
+    public float moveSp;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveSp, GetComponent<Rigidbody2D>().velocity.x);
+       
+    }
+   
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Plataforma"))
+        {
+            colidde = true;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Plataforma"))
+        {
+            colidde = false;
+
+        }
+
+    }
+}
