9294d89c37b96a45b9023fb18afc327cebdefd67
Guilherme Carini <carinigcontact@gmail.com>
2021-04-05 12:41:17 -0300

    Script das abas feito,falta impletemntar na unity,possui som ao clicar e soltar e fica mais claro se houver o clica e arrasta

--- assets/script/DragAndDrop.cs
+++ assets/script/DragAndDrop.cs
@@ -1,5 +1,39 @@
+using System.IO.Enumeration;
+using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Colleciontos;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
-    public class DragAndDrop
+    public class DragAndDrop : MonoBehaviour, IPointerDownGandler,IBeginDragHadler,IEndDragHandler,IEndDragHandler
     {
-        
+    [SerializeField] private ReactTransform _transform;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private AudioSource _as;
+    [SerializeField] private AudioClip _grab;
+    [SerializeField] private AudioClip _drop;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _as.clip = _grab;
+        _as.Play();
+        _canvasGroup.Alpha= 0.5f;
+       _canvasGroup.bloacksRaycasts = false;
     }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.Alpha = 1f;
+        _canvasGroup.bloacksRaycasts = true;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        _transform.anchoredPosition += eventData.delta/_canvas.scaleFactor;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Apertou");
+    }
+}
